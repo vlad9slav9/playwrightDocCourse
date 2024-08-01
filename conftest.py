95f0e6d5e3795b25fed61876f56db013be90170d
vlad vusaty <vladel9ts@gmail.com>
2024-08-01 21:02:10 +0300

    add authentication saving

--- conftest.py
+++ conftest.py
@@ -1,12 +1,12 @@
+import os
 import pytest
 import configparser
 
 config = configparser.ConfigParser()
-config.read('config.ini')
+config.read('auth.ini')
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default="gost",
-                     help="Choose browser: gost or yandex")
+    parser.addoption('--browser_name', action='store', default="gost", help="Choose browser: gost or yandex")
 
 @pytest.fixture(scope="session")
 def browser(playwright, request):
@@ -34,16 +34,25 @@ def unauth_page(browser):
 
 @pytest.fixture(scope="function")
 def auth_page(browser):
-    context = browser.new_context()
-    page = context.new_page()
 
-    username = config['credentials']['username']
-    password = config['credentials']['password']
+    if os.path.exists("auth.json"):
+        context = browser.new_context(storage_state="auth.json")
+        page = context.new_page()
+        page.goto("https://www.saucedemo.com/inventory.html")
 
-    page.goto("https://www.saucedemo.com")
-    page.locator("#user-name").fill(username)
-    page.locator("#password").fill(password)
-    page.locator("#login-button").click()
+    else:
+        context = browser.new_context()
+        page = context.new_page()
+
+        username = config['credentials']['username']
+        password = config['credentials']['password']
+
+        page.goto("https://www.saucedemo.com")
+        page.locator("#user-name").fill(username)
+        page.locator("#password").fill(password)
+        page.locator("#login-button").click()
+
+        context.storage_state(path="auth.json")
 
     yield page
 
--- test_examples.py
+++ test_examples.py
@@ -25,4 +25,9 @@ def test_add_cart(auth_page):
     auth_page.locator("[data-test=\"shopping-cart-link\"]").click()
     expect(auth_page.locator("[data-test=\"inventory-item-name\"]")).to_contain_text("Sauce Labs Backpack")
 
+def test_logout(auth_page):
+    auth_page.get_by_role("button", name="Open Menu").click()
+    auth_page.locator("[data-test=\"logout-sidebar-link\"]").click()
+    expect(auth_page.locator("[data-test=\"login-button\"]")).to_be_visible()
+
 
