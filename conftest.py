94d072bb7715a035d5a5aefc5bad30a7c2572e4d
vlad vusaty <vladel9ts@gmail.com>
2024-08-01 15:15:34 +0300

    add browser selection

--- conftest.py
+++ conftest.py
@@ -1,10 +1,50 @@
 import pytest
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default="gost",
+                     help="Choose browser: gost or yandex")
+
+@pytest.fixture(scope="session")
+def browser(playwright, request):
+    browser_name = request.config.getoption("browser_name")
+    #browser = None
+    if browser_name == "gost":
+        browser = playwright.chromium.launch(executable_path=r"C:\Program Files (x86)\krtech\chrome\chrome.exe", headless=False)
+    elif browser_name == "yandex":
+        browser = playwright.chromium.launch(executable_path=r"C:\Users\KSK-SHOP\AppData\Local\Yandex\YandexBrowser\Application\browser.exe", headless=False)
+    else:
+        raise pytest.UsageError("--browser_name should be gost or yandex")
+    yield browser
+    browser.close()
 
 @pytest.fixture(scope="function")
-def browser(playwright, browser_type):
+def unauth_page(browser):
+    context = browser.new_context()
+    page = context.new_page()
 
-    browser = playwright.chromium.launch(executable_path=r"C:\Program Files (x86)\krtech\chrome\chrome.exe", headless=False)
+    page.goto("https://www.saucedemo.com")
 
-    yield browser
+    yield page
 
-    browser.close()
+    context.close()
+
+@pytest.fixture(scope="function")
+def auth_page(browser):
+    context = browser.new_context()
+    page = context.new_page()
+
+    username = config['credentials']['username']
+    password = config['credentials']['password']
+
+    page.goto("https://www.saucedemo.com")
+    page.locator("#user-name").fill(username)
+    page.locator("#password").fill(password)
+    page.locator("#login-button").click()
+
+    yield page
+
+    context.close()
--- test_examples.py
+++ test_examples.py
@@ -1,12 +1,28 @@
 import re
 from playwright.sync_api import Page, expect
 
-def test_has_title(page: Page):
-    page.goto("https://playwright.dev/")
-    expect(page).to_have_title(re.compile("Playwright"))
 
-def test_get_started_link(page: Page):
-    page.goto("https://playwright.dev/")
-    page.get_by_role("link", name="Get started").click()
+def test_correct_login(unauth_page):
 
-    expect(page.get_by_role("heading", name="Installation123")).to_be_visible()
+    unauth_page.locator("#user-name").fill("standard_user")
+    unauth_page.locator("#password").fill("secret_sauce")
+    unauth_page.locator("#login-button").click()
+
+    expect(unauth_page.locator("[data-test=\"shopping-cart-link\"]")).to_be_visible()
+
+def test_incorrect_login(unauth_page):
+
+    unauth_page.locator("#user-name").fill("FalseName")
+    unauth_page.locator("#password").fill("secret_sauce")
+    unauth_page.locator("#login-button").click()
+
+    expect(unauth_page.locator("[data-test=\"error\"]")).to_contain_text(
+        "Epic sadface: Username and password do not match any user in this service")
+
+
+def test_add_cart(auth_page):
+    auth_page.locator("#add-to-cart-sauce-labs-backpack").click()
+    auth_page.locator("[data-test=\"shopping-cart-link\"]").click()
+    expect(auth_page.locator("[data-test=\"inventory-item-name\"]")).to_contain_text("Sauce Labs Backpack")
+
+
